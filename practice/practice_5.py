288edf303950f89f9d04afa78711f772ac86d08a
banyaofei <38749051+banyaofei@users.noreply.github.com>
2018-04-26 17:19:19 +0800

    Add files via upload

---
+++ practice/practice_5.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+'learn from https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431676242561226b32a9ec624505bb8f723d0027b3e7000'
+
+__author__ = 'banyaofei'
+
+# -*- coding: utf-8 -*-
+
+L = ['Bart', 'Lisa', 'Adam']
+
+for name in L:
+    print('Hello,%s!' % (name))
