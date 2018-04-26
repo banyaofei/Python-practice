35cfc5da1da5e3c8093e3b569fe47b5d33aef607
banyaofei <38749051+banyaofei@users.noreply.github.com>
2018-04-26 16:55:22 +0800

    Add files via upload

---
+++ practice/practice_2.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+'learn from https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431664106267f12e9bef7ee14cf6a8776a479bdec9b9000'
+
+__author__ = 'banyaofei'
+
+s1 = 72
+s2 = 85
+
+growth_rate = (s2-s1)/s1*100
+
+print('小明成绩提升了%.1f%%' % (growth_rate))
---
+++ practice/practice_3.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+'learn from https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/0014316724772904521142196b74a3f8abf93d8e97c6ee6000'
+
+__author__ = 'banyaofei'
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+# 打印Apple:
+print(L[0][0])
+# 打印Python:
+print(L[1][1])
+# 打印Lisa:
+print(L[2][2])
