f98aa825ff6662db6141043f424c8f6e643053d7
banyaofei <38749051+banyaofei@users.noreply.github.com>
2018-04-26 17:06:34 +0800

    Add files via upload

---
+++ practice/practice_4.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+'learn from https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431675624710bb20e9734ef343bbb4bd64bcd37d4b52000'
+
+__author__ = 'banyaofei'
+
+# -*- coding: utf-8 -*-
+
+height = 1.75
+weight = 80.5
+
+bmi = weight/height**2
+
+if bmi < 18.5:
+    print('过轻')
+elif bmi < 25:
+    print('正常')
+elif bmi < 28:
+    print('过重')
+elif bmi < 32:
+    print('肥胖')
+else:
+    print('严重肥胖')
