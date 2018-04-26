3fe981899cabf78928b536e06b0d08e3837b7e7d
banyaofei <38749051+banyaofei@users.noreply.github.com>
2018-04-26 17:28:01 +0800

    Add files via upload

---
+++ practice/practice_6.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+'learn from https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/0014316784721058975e02b46cc45cb836bb0827607738d000'
+
+__author__ = 'banyaofei'
+
+n1 = 255
+n2 = 1000
