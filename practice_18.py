50e7536c8085b01112ce3066ddc2881c474d03ea
banyaofei <38749051+banyaofei@users.noreply.github.com>
2018-05-09 09:09:11 +0800

    Add files via upload

---
+++ practice_18.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+'learn from https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/0014319117128404c7dd0cf0e3c4d88acc8fe4d2c163625000'
+
+__author__ = 'banyaofei'
+
+class Student(object):
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+        Student.count += 1
+
+
+# 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
