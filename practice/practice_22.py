eacea3064286bc53f48534f8366d9207c8f8c41e
banyaofei <38749051+banyaofei@users.noreply.github.com>
2018-05-17 16:34:04 +0800

    Add files via upload

---
+++ practice/practice_22.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+'learn from https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/00143191375461417a222c54b7e4d65b258f491c093a515000'
+
+__author__ = 'banyaofei'
+
+from functools import reduce
+
+def str2num(s):
+    try:
+        return int(s)
+    except:
+        return float(s)
+
+def calc(exp):
+    ss = exp.split('+')
+    ns = map(str2num, ss)
+    return reduce(lambda acc, x: acc + x, ns)
+
+def  main():
+    r = calc('100 + 200 + 345')
+    print('100 + 200 + 345 =', r)
+    r = calc('99 + 88 + 7.6')
+    print('99 + 88 + 7.6 =', r)
+
+main()
